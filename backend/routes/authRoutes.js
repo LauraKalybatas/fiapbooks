8c91c7a612268bf0dc945e25a2b9d322dc4c2b13
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-24 08:04:33 -0300

    future fest backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,59 +5,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 const Produto = require("../models/produtos");
 
-// registrando um usuário
-router.post("/register", async(req, res)=>{
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    //checando se todos os dados foram enviados
-    if (name==null || email ==null || password == null){
-        return res.status(400).json({error: "Por favor, preencha todos os campos"});
-    }
-
-    //conferindo se o usuário já existe
-    const emailExists = await User.findOne({email: email});
-
-    if(emailExists){
-        return res.status(400).json({error: "O e-mail informado já existe."})
-    }
-
-    //criando a senha com bcrypt
-    const salt = await bcrypt.genSalt(12);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    //criando o usuário após as validações no sistema
-    const user = new User({
-        name : name,
-        email : email,
-        password: passwordHash
-    });
-
-    //montando um try catch para pega outros erros e afins
-    try{
-        const newUser = await user.save();
-        
-        //criando o token do usuario
-        const token = jwt.sign(
-            //payload
-            {
-                name : newUser.name,
-                id : newUser._id
-            },
-            "segredo" //isso torna o nosso token único
-        );
-
-        //retornar o token para o projeto e manda mensagem
-        res.json({error: null, msg: "Você fez o cadastro com sucesso!!!", token: token, userId:
-        newUser._id});
-    } catch(error){
-        res.status(400).json({error});
-    }
-});
-
 //cadastro de produto
-router.post("/register", async(req, res)=>{
+router.post("/registerProduto", async(req, res)=>{
     const codigo = req.body.codigo;
     const descricao = req.body.descricao;
     const fornecedor = req.body.fornecedor;
@@ -69,14 +18,14 @@ router.post("/register", async(req, res)=>{
         return res.status(400).json({error: "Por favor, preencha todos os campos"});
     }
 
-    //conferindo se o usuário já existe
+    //conferindo se o produto já existe
     const produtoExists = await User.findOne({codigo: codigo});
 
     if(produtoExists){
         return res.status(400).json({error: "O produto informado já está cadastrado."})
     }
 
-    //criando o usuário após as validações no sistema
+    //criando o produto após as validações no sistema
     const produto = new Produto({
         codigo: codigo,
         descricao: descricao,
@@ -107,4 +56,4 @@ router.post("/register", async(req, res)=>{
     }
 });
 
-module.exports = router;
+module.exports= router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 //routes
 const authRouter = require("./routes/authRoutes");
@@ -10,39 +9,137 @@ const authRouter = require("./routes/authRoutes");
 //midllewares
 
 //config
-const dbName = "fiapbooks";
-const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+const port = 3000;
 
 //atrelar as rotas ao express
 app.use("/api/auth", authRouter);
 
 //conexão mongodb
 mongoose.connect(
-    `mongodb://127.0.0.1:27017/${dbName}`,{
+    `mongodb://127.0.0.1:27017/dbooks`,{
     useNewUrlParser : true,
     UseUnifiedTopology : true,
-    serverSelectionTimeoutMS : 20000
     }
 )
 
-//atrelando a primeira rota
-app.get("/",(req,res)=>{
-    res.json({message: "Rota de teste, será trocada!!!"});
+//criando a model do seu projeto
+const UsuarioSchema = new mongoose.Schema({
+  nome: { type: String },
+  email: { type: String, required: true },
+  password: { type: String }
 });
 
-//escutando a porta
-app.listen(port, ()=>{
-    console.log(`O backend está rodando na porta ${port}`)
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+
+//criando a model do seu projeto
+const ProdutoSchema = new mongoose.Schema({
+    codigo: {type: Number, required: true},
+    descricao: {type: String},
+    fornecedor: {type: String},
+    dataImpressao: {type: Date},
+    quantidadeEstoque: {type: Number}
+});
+  
+const Produto = mongoose.model("Produto", ProdutoSchema);
+
+//configuração dos roteamendos
+//cadastrousuario
+app.post("/cadastrousuario", async (req, res) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const password = req.body.password;
+
+
+  //validação de campos
+  if(nome == null || email == null || password == null){
+    return res.status(400).json({error : "Prencher todos os campos!!!"});
+  }
+
+  //teste de duplicidade
+  const emailExiste = await Usuario.findOne({email : email});
+
+  if(emailExiste){
+    return res.status(400).json({error : "O email informado já existe"});
+  }
+
+  const usuario = new Usuario({
+    nome: nome,
+    email: email,
+    password: password
+  });
+
+  try {
+    const newUsuario = await usuario.save();
+    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+  } catch (error) {}
 });
 
+app.post("/cadastroproduto", async (req, res) => {
+    const codigo = req.body.codigo;
+    const descricao = req.body.descricao;
+    const fornecedor = req.body.fornecedor;
+    const dataImpressao = req.body.dataImpressao;
+    const quantidadeEstoque = req.body.quantidadeEstoque
 
+    //checando se todos os dados foram enviados
+    if (codigo==null || descricao ==null || fornecedor == null || dataImpressao == null || quantidadeEstoque == null){
+        return res.status(400).json({error: "Por favor, preencha todos os campos"});
+    }
 
+    //conferindo se o produto já existe
+    const produtoExists = await User.findOne({codigo: codigo});
+
+    if(produtoExists){
+        return res.status(400).json({error: "O produto informado já está cadastrado."})
+    }
+
+    //criando o produto após as validações no sistema
+    const produto = new Produto({
+        codigo: codigo,
+        descricao: descricao,
+        fornecedor: fornecedor,
+        dataImpressao: dataImpressao,
+        quantidadeEstoque: quantidadeEstoque
+    });
+
+    //montando um try catch para pega outros erros e afins
+    try{
+        const newProduto = await produto.save();
+        
+        //criando o token do usuario
+        const token = jwt.sign(
+            //payload
+            {
+                codigo: newProduto.codigo,
+                id: newProduto._id
+            },
+            "segredo" //isso torna o nosso token único
+        );
+
+        //retornar o token para o projeto e manda mensagem
+        res.json({error: null, msg: "Você fez o cadastro com sucesso!!!", token: token, produtoId:
+        newProduto._id});
+    } catch(error){
+        res.status(400).json({error});
+    }
+});
+
+//rota de get de formulario
+app.get("/cadastrousuario", async (req, res) => {
+  res.sendFile(__dirname);
+});
+
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 
 
 
